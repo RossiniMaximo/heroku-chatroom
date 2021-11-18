01eccb5434a3b55a717ea2aee76cc7423aa5ef85
Maximo <maximorossini2016@gmail.com>
2021-11-17 21:15:04 -0300

    fifth-commit

--- Documents/src/dwf-m6/heroku-challengue/src/state.ts
+++ Documents/src/dwf-m6/heroku-challengue/src/state.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://dwf-m6-heroku.herokuapp.com/";
+const API_URL = "http://localhost:3000";
 import { rtdb } from "./rtdb";
 import map from "lodash/map"
 /* no se esta mandando el from de los mensajes dando undefined */
